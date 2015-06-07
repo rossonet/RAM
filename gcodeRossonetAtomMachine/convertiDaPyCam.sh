3c406a89943adfc676fa01cdec23fe055c52c0f3
Andrea Ambrosini -Rossonet s.c.a r.l.- <andrea.ambrosini@rossonet.com>
2015-06-07 22:17:40 +0200

    aggiunto gcode caffe

---
+++ gcodeRossonetAtomMachine/convertiDaPyCam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cp $1 temp.gcode
+#inizio file e centro
+sed -i "s/^ X/G1 X/" temp.gcode
+sed -i "s/^ Y/G1 Y/" temp.gcode
+sed -i "s/^;.*$//" temp.gcode
+sed -i "s/F200.00000/G21/" temp.gcode
+sed -i "s/S1000.00000/G28/" temp.gcode
+sed -i "s/T3 M6/G1 Z100 F10000/" temp.gcode
+sed -i "s/M3 (start spindle)/G92/" temp.gcode
+sed -i "s/G04 P3 (wait for 3 seconds)/G90/" temp.gcode
+
+sed -i "s/G0 Z25.0000/G1 Z25 F3000/" temp.gcode
+sed -i "s/G1 Z0\..*$/G1 Z11 F2500/" temp.gcode
+
+# Fine file
+sed -i "s/M5 (stop spindle)/G1 X0 Y0 Z200 F5000/" temp.gcode
+sed -i "s/M2 (end program)/G28/" temp.gcode
+cat temp.gcode | grep -v '^$' > elaborato_$1
+rm temp.gcode
+
+
