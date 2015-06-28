778d5f0b685b34b98814c0bfecf039aacb424b49
Andrea Ambrosini -Rossonet s.c.a r.l.- <andrea.ambrosini@rossonet.com>
2015-06-28 06:22:46 +0200

    secondo debug g28 + pulizia sensori z

--- Marlin/Marlin/Marlin_main.cpp
+++ Marlin/Marlin/Marlin_main.cpp
@@ -1851,7 +1851,7 @@ inline void gcode_G28() {
   saved_feedrate_multiplier = feedrate_multiplier;
   feedrate_multiplier = 100;
   refresh_cmd_timeout();
-
+  endstops_hit_on_purpose();  // by Ambrosini
   enable_endstops(true);
 
   set_destination_to_current();
